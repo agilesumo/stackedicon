5dc8db14525ff4d0ed4bdda94c56c5125a009d7f
Andy O'Connell <oconnell.andy@gmail.com>
2025-09-07 18:07:35 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,11 +23,12 @@ let canvas;
 
 // Load the image.
 function preload() {
-  imgWashMachine = loadImage("/assets/washing_machine.png");
-  imgToilet = loadImage("/assets/toilet.png");
-  imgShower = loadImage("/assets/shower.png");
-  imgTap = loadImage("/assets/tap.png");
-  imgTub = loadImage("/assets/bathtub.png");
+  imgWashMachine = loadImage("https://drive.usercontent.google.com/download?id=1Q0oARsnHwWWxT-3U3expupSgo-2udy7e");
+  imgToilet = loadImage("https://drive.usercontent.google.com/download?id=1sS3COBKyc_nHiy02Jh3-YlUiKSrEReDX");
+  imgShower = loadImage("https://drive.usercontent.google.com/download?id=1xxQFN2-lcH_KaNzReB_9MFHJT603SN57");
+  imgTap = loadImage("https://drive.usercontent.google.com/download?id=1YX1v7fnGV3wKkNceFaIz1JzmP3y5JuEU");
+  imgTub = loadImage("https://drive.usercontent.google.com/download?id=1fPwkl5ogUjhoKLTS3eWj9svix2fq99_y");
+
 }
 
 function setup() {
